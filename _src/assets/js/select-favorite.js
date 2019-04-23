66482ac3c70fffbd93f29a5a6f5868cc00a989ef
veroena <veronica.enamorado@gmail.com>
2019-04-23 13:26:56 +0200

    First try close button

--- _src/assets/js/select-favorite.js
+++ _src/assets/js/select-favorite.js
@@ -33,11 +33,13 @@ function paintFav(array) {
       const favItem = document.createElement('li');
       const favItemTitle = document.createElement('h4');
       const favItemImage = document.createElement('img');
+      const favItemClose = document.createElement('i');
   
        //añado clases a los elementos
        favItem.classList.add('fav-item');
        favItemTitle.classList.add('fav-item__title');
        favItemImage.classList.add('fav-item__image');
+       favItemClose.classList.add('fas', 'fa-times-circle');
   
        //creo contenido
        const nameShowFav = array[i].name;
@@ -56,9 +58,20 @@ function paintFav(array) {
        //añado elementos dentro de otros elementos
        favItem.appendChild(favItemImage);
        favItem.appendChild(favItemTitle);
+       favItem.appendChild(favItemClose);
   
        //añado elementos al HTML
        favList.appendChild(favItem);
+
+      // favItemClose.addEventListener('click', deleteFav);
     }
   }
 }
+
+// function deleteFav(event) {
+//   const find = favArr.findIndex(show => show.id === event.currentTarget.id);
+//   if (event.currentTarget.id === ) {
+//     favArr.splice(find, 1);
+//   }
+//   saveArr(favArr);
+// }
